27d14bd2b9716ce168326c3c5539c163bfc62d65
liulin4532617@163.com <liulin4532617@163.com>
2018-12-15 15:56:09 +0800

    集群配置

--- config-client/src/main/java/com/itliulin/configclient/controller/ProductController.java
+++ config-client/src/main/java/com/itliulin/configclient/controller/ProductController.java
@@ -1,5 +1,10 @@
 package com.itliulin.configclient.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductController {
 
-    private  String profile;
+    @Value("${version}")
+    private String version;
 
-    @RequestMapping("/hello")
-    public String hello(){
-        return null;
+    @Value("${name}")
+    private String name;
+
+    @Value("${age}")
+    private String age;
+
+    @GetMapping("/hello")
+    public Object hello() {
+        Map map = new HashMap();
+        map.put("age", age);
+        map.put("name", name);
+        map.put("version", version);
+        return ResponseEntity.ok(map);
     }
 }
