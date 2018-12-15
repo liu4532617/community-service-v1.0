0cc3de6134ff0c3a7b9d6d3ab64610f1dd59fe9a
liulin4532617@163.com <liulin4532617@163.com>
2018-12-15 17:36:23 +0800

    集群配置

---
+++ config-server/src/main/java/com/ymkj/configserver/TestController.java
@@ -0,0 +1,12 @@
+package com.ymkj.configserver;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liulin
+ * @date 2018/12/15
+ */
+@RestController
+public class TestController {
+
+}
